e932bdb747accd97d52f435bd47ee91a10649a30
1054 <liudz1054@gmail.com>
2019-11-28 03:42:42 +0100

    Updates

--- bin/alma_archive_run_tclean_for_continuum.py
+++ bin/alma_archive_run_tclean_for_continuum.py
@@ -54,7 +54,7 @@ def go(vis):
         
         field = str(field).strip()
         
-        final_output_image = '%s_SB_%s_GB_%s_MB_%s_%s_sci.spw%s.cont.I.image.fits'%(project, SB, GB, MB, field, '_'.join(np.array(spw_ids).astype(str).tolist())), 
+        final_output_image = '%s_SB_%s_GB_%s_MB_%s_%s_sci.spw%s.cont.I.image.fits'%(project, SB, GB, MB, field, '_'.join(np.array(spw_ids).astype(str).tolist()))
         print('final_output_image = %s'%(final_output_image))
         
         if not os.path.isfile(final_output_image):
