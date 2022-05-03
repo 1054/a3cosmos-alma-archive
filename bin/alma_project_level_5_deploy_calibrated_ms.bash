3459ff7d4ed07d1706b4593b0374e6602ccca4b7
1054 <>
2022-05-03 13:05:07 +0200

    Updates

--- bin/alma_project_level_5_deploy_calibrated_ms.bash
+++ bin/alma_project_level_5_deploy_calibrated_ms.bash
@@ -94,7 +94,7 @@ if [[ ! -d Level_1_Raw/${Project_code} ]]; then
 fi
 
 
-# make Deploy_dir directory
+# remove Deploy_dir directory suffix
 if [[ "$Deploy_dir" == *"/" ]]; then
     Deploy_dir=$(echo "$Deploy_dir" | sed -e 's%/$%%g')
 fi
@@ -141,14 +141,14 @@ fi
 
 
 # search for files, they must comply certain naming rules
-echo_output "Searching for \"Level_1_Raw/${Project_code}/s*/g*/m*/calibrated.ms\" ..."
+echo_output "Searching for \"Level_1_Raw/${Project_code}/s*/g*/m*/calibrated/calibrated.ms\" ..."
 list_files=($(find "Level_1_Raw/${Project_code}" -mindepth 5 -maxdepth 5 -type d -name "calibrated.ms"))
 if [[ ${#list_files[@]} -eq 0 ]]; then
-    echo_output "Searching for \"Level_1_Raw/${Project_code}/s*/g*/m*/uid__*.ms\" ..."
+    echo_output "Searching for \"Level_1_Raw/${Project_code}/s*/g*/m*/calibrated/uid__*.ms\" ..."
     list_files=($(find "Level_1_Raw/${Project_code}" -mindepth 5 -maxdepth 5 -type d -name "uid__*.ms"))
 fi
 if [[ ${#list_files[@]} -eq 0 ]]; then
-    echo_output "Searching for \"Level_1_Raw/${Project_code}/s*/g*/m*/uid__*.ms.split.cal\" ..."
+    echo_output "Searching for \"Level_1_Raw/${Project_code}/s*/g*/m*/calibrated/uid__*.ms.split.cal\" ..."
     list_files=($(find "Level_1_Raw/${Project_code}" -mindepth 5 -maxdepth 5 -type d -name "uid__*.ms.split.cal"))
 fi
 if [[ ${#list_files[@]} -eq 0 ]]; then
